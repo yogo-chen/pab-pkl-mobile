4ff6495fffd0b93656f66284b448ed55598154f1
yogo-chen <prayogocendra@gmail.com>
2017-02-15 20:48:12 +0700

    Update register layout

--- app/src/main/java/com/cendra/prayogo/pklmobile/RegisterActivity.java
+++ app/src/main/java/com/cendra/prayogo/pklmobile/RegisterActivity.java
@@ -1,14 +1,56 @@
 package com.cendra.prayogo.pklmobile;
 
+import android.app.DatePickerDialog;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.TimeZone;
 
 public class RegisterActivity extends AppCompatActivity {
+    private EditText birthdayEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        this.birthdayEditText = (EditText) findViewById(R.id.register_birthdayEditText);
+        this.birthdayEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setBirthdayDate();
+            }
+        });
     }
+
+    private void setBirthdayDate() {
+        final Calendar calendar = Calendar.getInstance(TimeZone.getDefault().getDefault());
+        DatePickerDialog.OnDateSetListener dateListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                calendar.set(Calendar.YEAR, year);
+                calendar.set(Calendar.MONTH, monthOfYear);
+                calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+                SimpleDateFormat dateFormat = new SimpleDateFormat("MMMM d, y");
+                birthdayEditText.setText(dateFormat.format(calendar.getTime()));
+            }
+        };
+        DatePickerDialog datePickerDialog = new DatePickerDialog(
+                RegisterActivity.this,
+                dateListener,
+                calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH)
+        );
+        datePickerDialog.show();
+    }
+
 }
