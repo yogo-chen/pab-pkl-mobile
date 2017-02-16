736b17663edfcdacc8df2bc4a5d23af80e7e6d78
yogo-chen <prayogocendra@gmail.com>
2017-02-16 08:09:36 +0700

    Update layout and string relocation

--- app/src/main/java/com/cendra/prayogo/pklmobile/RegisterActivity.java
+++ app/src/main/java/com/cendra/prayogo/pklmobile/RegisterActivity.java
@@ -4,6 +4,8 @@ import android.app.DatePickerDialog;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.DatePicker;
@@ -14,20 +16,54 @@ import java.util.Calendar;
 import java.util.TimeZone;
 
 public class RegisterActivity extends AppCompatActivity {
+    private EditText emailEditText;
+    private EditText passwordEditText;
+    private EditText rePasswordEditText;
+    private EditText nameEditText;
+    private EditText addressEditText;
+    private EditText phoneEditText;
     private EditText birthdayEditText;
+    private EditText featuredProductEditText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        this.emailEditText = (EditText) findViewById(R.id.register_emailEditText);
+        this.passwordEditText = (EditText) findViewById(R.id.register_passwordEditText);
+        this.rePasswordEditText = (EditText) findViewById(R.id.register_rePasswordEditText);
+        this.nameEditText = (EditText) findViewById(R.id.register_nameEditText);
+        this.addressEditText = (EditText) findViewById(R.id.register_addressEditText);
+        this.phoneEditText = (EditText) findViewById(R.id.register_phoneEditText);
         this.birthdayEditText = (EditText) findViewById(R.id.register_birthdayEditText);
+        this.featuredProductEditText = (EditText) findViewById(R.id.register_featuredProductEditText);
+
+        this.emailEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
         this.birthdayEditText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 setBirthdayDate();
             }
         });
+
     }
 
     private void setBirthdayDate() {
@@ -53,4 +89,35 @@ public class RegisterActivity extends AppCompatActivity {
         datePickerDialog.show();
     }
 
+    private String getEmail() {
+        return this.emailEditText.getText().toString();
+    }
+
+    private String getPassword() {
+        return this.passwordEditText.getText().toString();
+    }
+
+    private String getRePassword() {
+        return this.rePasswordEditText.getText().toString();
+    }
+
+    private String getName() {
+        return this.nameEditText.getText().toString();
+    }
+
+    private String getAddress() {
+        return this.addressEditText.getText().toString();
+    }
+
+    private String getPhone() {
+        return this.phoneEditText.getText().toString();
+    }
+
+    private String getBirthday() {
+        return this.birthdayEditText.getText().toString();
+    }
+
+    private String getFeaturedProduct() {
+        return this.featuredProductEditText.getText().toString();
+    }
 }
